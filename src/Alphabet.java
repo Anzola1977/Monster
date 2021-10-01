ab5289d37afa7abb549744b1dcfd240f54fe09ec
Andrei Zola <anzola@yandex.ru>
2021-10-01 11:40:35 +0200

    Alphabet

---
+++ src/Alphabet.java
@@ -0,0 +1,44 @@
+public class Alphabet {
+    private int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, sum;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+    public int getSum() {
+        return a + b + c + d + e + f + g + h + i + j + k + l +
+                m + n + o + p + q + r + s + t + u + v +
+                w + x + y + z;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
